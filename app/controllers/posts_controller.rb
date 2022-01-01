cb9b48b019634ed3b3818f3543f3dbccbb206565
h-ucchi <h-ucchi+study@github.com>
2022-01-01 15:14:14 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   #end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index #メモを保存した後にトップページにリダイレクトされるようにする（自動でトップページに戻る処理）
+    post = Post.create(content: params[:content])
+    render json:{ post: post}
   end
 end
